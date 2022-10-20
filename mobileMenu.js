c6ddbd2ab4951db72520735a71e120b025d91b58
Marienela Munoz Gutierrez <marienelamunozgutierrez@MacBook-Pro-de-Marienela.local>
2022-10-20 13:10:45 -0400

    Add responsive about page and interaction

--- mobileMenu.js
+++ mobileMenu.js
@@ -3,14 +3,14 @@ const mobileMenu = document.querySelector('.mobile-menu');
 const xButton = document.querySelector('.container-x');
 
 navBarMobile.addEventListener('click', () => {
-  navBarMobile.classList.toggle('active');
+  navBarMobile.classList.toggle('disabled');
   mobileMenu.classList.toggle('active');
+  xButton.classList.remove('disabled');
 });
 
 xButton.addEventListener('click', () => {
-  xButton.classList.toggle('active');
   mobileMenu.classList.remove('active');
-  navBarMobile.classList.remove('active');
+  navBarMobile.classList.remove('disabled');
 });
 
 
